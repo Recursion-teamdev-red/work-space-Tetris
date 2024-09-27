66e669cb7a329540a43bda86e85079f1f7f63b53
Tai <taiu.engineer@gmail.com>
2024-09-28 01:34:57 +0900

    fix scoring #33

--- js/game/scoring.js
+++ js/game/scoring.js
@@ -18,9 +18,8 @@ export function arenaSweep(arena, state) {
 }
 
 function arenaSweepHelper(y, state, rowCount, arena) {
-  
   // arenaの先頭まで走査したら終了
-  if(y <= 0) return;
+  if (y <= 0) return;
 
   // 各行の全てのセルをチェック
   for (let x = 0; x < arena[y].length; ++x) {
@@ -30,7 +29,7 @@ function arenaSweepHelper(y, state, rowCount, arena) {
       return arenaSweepHelper(y, state, rowCount, arena);
     }
   }
-  
+
   // 行が全て埋まっている場合の処理
   // その行を取り除き、新しい空の行を上に追加
   arena.splice(y, 1); // 行を削除
@@ -42,6 +41,8 @@ function arenaSweepHelper(y, state, rowCount, arena) {
 
   // 次の行を消去した場合のスコア倍率を2倍に
   rowCount *= 2;
+
+  checkLevelUp(state);
 
   // spliceによりyインデックスがずれているので、再度同じインデックス番号で実行
   return arenaSweepHelper(y, state, rowCount, arena);
